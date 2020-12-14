c5bf2137a3f8dc5ef660f193b3e3b2e77217352d
Mariya <zim.masha@gmail.com>
2020-12-14 20:51:14 +0300

    3.2. Фиксация изменений на github. [#243564]

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,11 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] agrs) {
+        String idea = "I like Java";
+        idea = idea + "But I am a newbie.";
+        int year = 2020;
+        idea = idea + year;
+        System.out.println(idea);
+    }
+}
