7c8156fac76285257be4959e1f0fcd6941502af3
Mariya <zim.masha@gmail.com>
2020-12-16 11:08:34 +0300

    3.2. Фиксация изменений на github. [#243564]

---
+++ test/ru/job4j/calculator/FitTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.calculator;
+
+import org.junit.Test;
+
+import org.junit.Assert;
+
+public class FitTest {
+
+    @Test
+    public void whenMan180Then() {
+        short in = 180;
+        double expected = 92;
+        double out = Fit.manWeight(in);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenMan170Then() {
+        short in = 170;
+        double expected = 80.5;
+        double out = Fit.manWeight(in);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenWoman170Then() {
+        short in = 170;
+        double expected = 69;
+        double out = Fit.womanWeight(in);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}
