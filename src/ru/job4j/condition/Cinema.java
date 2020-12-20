18393806157bd9d7c140a41302ba97c097d5004b
Mariya <zim.masha@gmail.com>
2020-12-20 17:09:10 +0300

    3.2. Фиксация изменений на github. [#243564]

--- src/ru/job4j/condition/Cinema.java
+++ src/ru/job4j/condition/Cinema.java
@@ -1,6 +1,16 @@
 package ru.job4j.condition;
 
 public class Cinema {
+    public static void access(int age) {
+        System.out.println("The age of the customer is : " + age);
+        if (age > 18) {
+            System.out.println("Welcome to the cinema.");
+        }
+        else {
+            System.out.println("It is not for you.");
+        }
+    }
+
     public static void permission(boolean allowByParent, boolean hasMoney) {
         if (allowByParent && hasMoney) {
             System.out.println("I can go to the cinema.");
@@ -10,10 +20,12 @@ public class Cinema {
         }
     }
     public static void main(String[] args) {
-     permission(true, true);
+        Cinema.access(21);
+        Cinema.access(16);
+        permission(true, true);
         permission(true, false);
-        permission(false, true);
         permission(false, false);
+        permission(false, true);
     }
 }
 
