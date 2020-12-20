7d620b764ba76e2d8e02b26579124c032765c202
Mariya <zim.masha@gmail.com>
2020-12-20 22:11:22 +0300

    3.2. Фиксация изменений на github. [#243564]

---
+++ src/ru/job4j/condition/Triangle.java
@@ -0,0 +1,12 @@
+package ru.job4j.condition;
+
+public class Triangle {
+    public static boolean exist(double ab, double bc, double ac) {
+        return ((ab + bc) > ac) && ((ac + bc) >ab) && ((ab + ac) > bc);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(exist(2.2, 5, 3));
+        System.out.println(exist(10, 50, 30));
+    }
+}
