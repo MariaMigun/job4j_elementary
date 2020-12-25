93915c720e00c459d6beae5d76c211e5f5d7251f
Mariya <zim.masha@gmail.com>
2020-12-25 09:33:47 +0300

    3.2. Фиксация изменений на github. [#243564]

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,10 +2,7 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean prime = true;
-        if (number == 1) {
-           prime = false;
-        }
+        boolean prime = number > 1;
         for (int index = 2; index < number; index++) {
             if ((number % index) == 0) {
                 prime = false;
@@ -15,6 +12,6 @@ public class CheckPrimeNumber {
         return prime;
     }
     public static void main(String[] args) {
-        System.out.println(check(1));
+        System.out.println(check(5));
     }
 }
