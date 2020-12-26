3b1d59f60cc908c7e1fafe65f716363581e7ce6c
Mariya <zim.masha@gmail.com>
2020-12-26 13:44:22 +0300

    3.2. Фиксация изменений на github. [#243564]

--- src/ru/job4j/array/ArrayDefenition.java
+++ src/ru/job4j/array/ArrayDefenition.java
@@ -8,5 +8,12 @@ public class ArrayDefenition {
         System.out.println("Размер массива равен: " + surnames.length);
         float [] prices = new float[40];
         System.out.println("Размер массива равен: " + prices.length);
+        String [] names = new String[4];
+        names[0] = "John Wane";
+        names[1] = "John Lennon";
+        names[2] = "Charly Manson";
+        names[3] = "Kurt Cobain";
+        System.out.println(names[0] + "; " + names[1] + "; " + names[2] + "; " + names[3]);
+
     }
 }
