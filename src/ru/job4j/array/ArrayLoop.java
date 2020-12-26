b1a0e3f6b9e9535cdcecc8b9f9b6a6e44e06d71b
Mariya <zim.masha@gmail.com>
2020-12-26 14:01:05 +0300

    3.2. Фиксация изменений на github. [#243564]

--- src/ru/job4j/array/ArrayLoop.java
+++ src/ru/job4j/array/ArrayLoop.java
@@ -2,12 +2,12 @@ package ru.job4j.array;
 
 public class ArrayLoop {
     public static void main(String[] args) {
-        int [] funcValue = new int[5];
-        for (int index = 0; index < funcValue.length; index++){
-            funcValue[index] = index * 2 + 3;
+        int [] funcValues = new int[5];
+        for (int index = 0; index < funcValues.length; index++){
+            funcValues[index] = index * 2 + 3;
         }
-        for (int index = 0; index < funcValue.length; index++){
-            System.out.println(funcValue[index]);
+        for (int element : funcValues){
+            System.out.println(element);
         }
     }
 }
