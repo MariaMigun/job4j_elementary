6f2de9f5938bab22df20dc42607081081cae74e2
Mariya <zim.masha@gmail.com>
2020-12-28 23:58:26 +0300

    3.2. Фиксация изменений на github. [#243564]

--- src/ru/job4j/array/AlgoArray.java
+++ src/ru/job4j/array/AlgoArray.java
@@ -13,9 +13,7 @@ public class AlgoArray {
         array[3] = array[4];
         array[4] = temp;
         for (int index = 0; index < array.length; index++) {
-            if (index % 2 == 0) {
-                System.out.println(array[index]);
-            }
+            System.out.println(array[index]);
         }
     }
 }
