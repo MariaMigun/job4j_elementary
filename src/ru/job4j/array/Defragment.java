a586bb2418c63c902823e71529bf46b9d6a962b5
Mariya <zim.masha@gmail.com>
2020-12-29 23:22:50 +0300

    3.2. Фиксация изменений на github. [#243564]

---
+++ src/ru/job4j/array/Defragment.java
@@ -0,0 +1,31 @@
+package ru.job4j.array;
+
+public class Defragment {
+    public static String[] compress(String[] array) {
+        for (int index = 0; index < array.length; index++) {
+            if (array[index] == null) {
+                int point = index; /* указатель на null ячейку. */
+                for(int j = point + 1; j < array.length; j++) {
+                    if (array[j] != null) {
+                        String temp = array[point];
+                        array[point] = array[j];
+                        array[j] = temp;
+                        break;
+                    }
+                }
+                /* переместить первую не null ячейку. Нужен цикл. */
+            }
+            System.out.print(array[index] + " ");
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
+        String[] compressed = compress(input);
+        System.out.println();
+        for (int index = 0; index < compressed.length; index++) {
+            System.out.print(compressed[index] + " ");
+        }
+    }
+}
