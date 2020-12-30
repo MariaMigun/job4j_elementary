beb9cc45107c8539a49f85f8f3664018dc61508c
Mariya <zim.masha@gmail.com>
2020-12-30 14:51:00 +0300

    3.2. Фиксация изменений на github. [#243564]

--- src/ru/job4j/array/MatrixCheck.java
+++ src/ru/job4j/array/MatrixCheck.java
@@ -34,8 +34,8 @@ public class MatrixCheck {
     public static boolean isWin(char[][] board) {
         boolean result = false;
         for (int index = 0; index < board.length; index++) {
-            if (board[index][index] == 'X') {
-                result = MatrixCheck.monoHorizontal(board, index) || MatrixCheck.monoVertical(board, index);
+            if (MatrixCheck.monoHorizontal(board, index) || MatrixCheck.monoVertical(board, index)) {
+                result = true;
                 break;
             }
         }
