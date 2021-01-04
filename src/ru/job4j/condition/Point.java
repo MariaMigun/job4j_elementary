5a9c81691be8af059374b6a23926d7cf1d71a034
Mariya <zim.masha@gmail.com>
2021-01-04 15:59:48 +0300

    3.2. PointObj

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -1,20 +1,27 @@
 package ru.job4j.condition;
 
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
+
 public class Point {
-    public static double distance(int x1, int y1, int x2, int y2) {
-        double rsl1 = x1 - x2;
-        double rsl2 = y1 - y2;
-        double third = Math.pow(rsl1, 2);
-        double fourth = Math.pow(rsl2, 2);
-        double fifth = third + fourth;
-        double sixth = Math.sqrt(fifth);
-        return sixth;
+
+    private int x;
+
+    private int y;
+
+    public Point(int first, int second) {
+        this.x  = first;
+        this.y  = second;
+    }
+
+    public double distance(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
     }
 
     public static void main(String[] args) {
-        double result1 = Point.distance(0, 0, 2, 0);
-        System.out.println("result (0,0) to (2, 0) " + result1);
-        double result2 = Point.distance(1, 2, 3, 4);
-        System.out.println("result (0,0) to (2, 0) " + result2);
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double dist = a.distance(b);
+        System.out.println(dist);
     }
 }
